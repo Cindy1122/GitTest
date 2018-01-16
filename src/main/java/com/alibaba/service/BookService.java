d5294473802b6e24552cf04dc652fe102859a3cd
Yuan Jie <1577626542@qq.com>
2018-01-16 21:35:02 +0800

    yuanjie Commit

---
+++ src/main/java/com/alibaba/service/BookService.java
@@ -0,0 +1,19 @@
+package com.alibaba.service;
+
+import java.util.List;
+
+import com.alibaba.domain.Book;
+
+
+/**
+ * Book服务层接口
+ * */
+public interface BookService {
+	
+	/**
+	 * 查找所有图书
+	 * @return Book对象集合
+	 * */
+	List<Book> getAll();
+
+}
